7dda26233a52f32ef35fcdebdd01e7168e155b07
Caromin <c.aromin.unf@gmail.com>
2017-10-22 01:04:41 -0400

    fixed sql schema and seed data, work on handlebars next

--- db/schema.sql
+++ db/schema.sql
@@ -2,10 +2,11 @@ CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
-CREATE TABLE burgers(
+# If the table already exists, remove it before trying to create the table again
+DROP TABLE IF EXISTS burgers;
+
+CREATE TABLE burgers (
 id INT(10) AUTO_INCREMENT PRIMARY KEY,
-burger_name VARCHAR(40),
-devoured boolean NOT NULL default 0,
--- found online, says when something is changed in the row the timestamp will also change
-date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-)
+burger_name VARCHAR(255) NOT NULL,
+devoured boolean NOT NULL default 0
+);
